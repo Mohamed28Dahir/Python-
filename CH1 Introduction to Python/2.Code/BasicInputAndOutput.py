c1124c88ad64c4539813c5f8c4e2339d8715c55d
Mohamed28Dahir <mohameddahirosman26@gmail.com>
2025-08-19 19:43:28 +0300

    Add files via upload

---
+++ CH1 Introduction to Python/2.Code/BasicInputAndOutput.py
@@ -0,0 +1,8 @@
+#1. Getting input from the user:
+name = input("Enter your name: ")
+#Printing the input back to the user
+print("Magaceyga waa  " + name )\
+
+#2. Displaying output to the user:
+print("the Answer is 43")
+
