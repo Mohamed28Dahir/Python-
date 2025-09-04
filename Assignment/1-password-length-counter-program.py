635cbfbdca83366c131090837a5ce6947ac3c43a
Mohamed28Dahir <mohameddahirosman26@gmail.com>
2025-09-04 17:51:18 +0300

    Add files via upload

---
+++ Assignment/1-password-length-counter-program.py
@@ -0,0 +1,8 @@
+name = input('Soo Gali Magacaaga: ')
+password = input('Soo Gali Password: ')
+
+hidden_password = '*' * len(password)
+
+len_pass = len(password)
+
+print(f'ASC, {name} Password Waa {hidden_password} is {len_pass} Xaraf')
